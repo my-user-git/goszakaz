cfde8a9857ae7d9a63ad7df426b69b4b41c19e60
geekbraines <morozovol@yandex.ru>
2024-11-22 00:54:34 +0300

    last

--- js/script.js
+++ js/script.js
@@ -1,9 +1,14 @@
 document.addEventListener('DOMContentLoaded', function () {
     document.querySelector('.main-content__button').addEventListener('click', function () {
+        const windowInnerWidth = window.innerWidth;
         document.querySelectorAll('.main-content__article section').forEach(item => {
             const hidden = getComputedStyle(item);
             if (hidden.display === 'none') {
-                item.style.display = 'grid';
+                if (windowInnerWidth <= 600) {
+                    item.style.display = 'flex';
+                } else {
+                    item.style.display = 'grid';
+                }
             }
         })
     })
